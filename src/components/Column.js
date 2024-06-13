7cfa008ad6e70e4c205a089ab0f21f55b214e598
dhruv.mehta <mehta.dhruv@hindgreco.com>
2024-06-13 09:33:20 +0530

    drag & drop feature added

--- src/components/Column.js
+++ src/components/Column.js
@@ -4,21 +4,18 @@ import Task from "./Task";
 const Column = ({ title, tasks, id }) => {
   // console.log("tasks 123", tasks);
   return (
-    <div className="bg-slate-700 rounded-lg w-80 h-96 border border-gray-500">
-      <h1 className="sticky p-2 bg-orange-800  text-center ">{title}</h1>
+    <div className="bg-white relative overflow-y-scroll rounded-lg w-80 h-96 border border-solid border-black">
+      <h1 className="p-2 sticky bg-lime-600  text-center ">{title}</h1>
       <Droppable droppableId={id}>
         {(provided, snapshot) => (
           <div
-            className="p-1 bg-green-400 flex-grow rounded-xl shadow-lg min-h-24 "
+            className="p-1  flex-grow rounded-lg shadow-2xl min-h-24 "
             ref={provided.innerRef}
             {...provided.droppableProps}
             isdraggingover={snapshot.isDraggingOver}
           >
             {/* {console.log("tasks before map", tasks)} */}
-            {tasks.map((task, index) => {
-              // console.log("task in map", task);
-              <Task key={index} tas={task} index={index} />;
-            })}
+            {tasks.map((task, index) => <Task key={index} task={task} index={index} />)}
 
             {provided.placeholder}
           </div>
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,13 +1,13 @@
 import { Draggable } from "react-beautiful-dnd";
 
 const Task = ({ task, index }) => {
-  console.log("task 987", task.title);
+  // console.log("task 987", task.title);
   return (
     <Draggable draggableId={`${task.id}`} key={task.id} index={index}>
       {
       (provided, snapshot) => (
         <div
-          className="rounded-lg p-2  shadow-lg decoration-black mb-2 min-h-20 ml-2 mr-2 cursor-pointer flex justify-between flex-col "
+          className="rounded-lg p-4 bg-teal-600  shadow-lg decoration-black mb-2 min-h-20 ml-2 mr-2 cursor-pointer flex justify-between flex-col "
           {...provided.draggableProps}
           {...provided.dragHandleProps}
           ref={provided.innerRef}
@@ -19,7 +19,7 @@ const Task = ({ task, index }) => {
             </span>
           </div>
           <div className="flex justify-center p-2 ">
-            <div className="text-black">task</div>
+            <div className="">{task.title}</div>
           </div>
           {provided.placeholder}
         </div>
